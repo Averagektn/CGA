09d59f297e8a457cbb1560fe34583276285168ee
KotVMeshke <dimon2003mail.ru37@gmail.com>
2024-02-06 18:35:25 +0300

    Matrix transformations were added

--- lab1/ObjVisualizer/GraphicsComponents/MatrixOperator.cs
+++ lab1/ObjVisualizer/GraphicsComponents/MatrixOperator.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace ObjVisualizer.GraphicsComponents
 {
     internal class MatrixOperator
     {
+
+        public static Matrix4x4 Scale(Vector3 scale)
+        {
+            return new Matrix4x4(scale.X, 0, 0, 0,
+                                0, scale.Y, 0, 0,
+                                0, 0, scale.Z, 0,
+                                0, 0, 0, 1.0f);
+        }
+
+        public static Matrix4x4 Move(Vector3 transition)
+        {
+            return new Matrix4x4(0, 0, 0, transition.X,
+                                0,0, 0, transition.Y,
+                                0, 0, 0, transition.Z,
+                                0, 0, 0, 1.0f);
+        }
         public static Matrix4x4 GetModelMatrix()
         {
-            return new Matrix4x4();
+            return new Matrix4x4(1.0f, 0, 0, 0,
+                                 0, 1.0f, 0, 0,
+                                 0, 0, 1.0f, 0,
+                                 0, 0, 0, 1.0f);
         }
         public static Matrix4x4 GetViewMatrix(Camera camera)
         {
@@ -21,21 +35,22 @@ namespace ObjVisualizer.GraphicsComponents
             return new Matrix4x4(XAxis.X, XAxis.Y, XAxis.Z, -Vector3.Dot(XAxis, camera.Eye),
                                  YAxis.X, YAxis.Y, YAxis.Z, -Vector3.Dot(YAxis, camera.Eye),
                                  ZAxis.X, ZAxis.Y, ZAxis.Z, -Vector3.Dot(ZAxis, camera.Eye),
-                                 0, 0, 0, 1);
+                                 0, 0, 0, 1.0f);
         }
         public static Matrix4x4 GetProjectionMatrix(Camera camera)
         {
-            return new Matrix4x4(1 / (camera.Aspect * (float)Math.Tan(camera.FOV >> 2)), 0, 0, 0,
-                                 0, 1 / ((float)Math.Tan(camera.FOV >> 2)), 0, 0,
+            return new Matrix4x4(1.0f / (camera.Aspect * (float)Math.Tan(camera.FOV / 2.0f)), 0, 0, 0,
+                                 0, 1.0f / ((float)Math.Tan(camera.FOV / 2.0f)), 0, 0,
                                  0, 0, camera.ZFar / (camera.ZNear - camera.ZFar), (camera.ZNear * camera.ZFar) / (camera.ZNear - camera.ZFar),
-                                 0, 0, -1, 0);
+                                 0, 0, -1.0f, 0);
+
         }
         public static Matrix4x4 GetViewPortMatrix(int Width, int Height)
         {
-            return new Matrix4x4(Width >> 2, 0, 0, Width >> 2,
-                                 0, -(Height >> 2), 0, Height >> 2,
-                                 0, 0, 1, 0,
-                                 0, 0, 0, 1);
+            return new Matrix4x4(Width >> 1, 0, 0, (Width - 1) >> 1,
+                                 0, -(Height >> 1), 0, (Height - 1) >> 1,
+                                 0, 0, 1.0f, 0,
+                                 0, 0, 0, 1.0f);
         }
 
     }
