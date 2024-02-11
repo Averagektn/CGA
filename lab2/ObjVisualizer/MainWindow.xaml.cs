15f9cb33f208249b102259a8cf9c319c20a2490c
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-02-11 13:22:52 +0300

    Blue borders

--- lab2/ObjVisualizer/MainWindow.xaml.cs
+++ lab2/ObjVisualizer/MainWindow.xaml.cs
@@ -183,7 +183,12 @@ namespace ObjVisualizer
                         if (Vector3.Dot(PoliNormal / FaceNormales.Count, new Vector3(Vertexes[FaceVertexes[0] - 1].X,
                             Vertexes[FaceVertexes[0] - 1].Y, Vertexes[FaceVertexes[0] - 1].Z) + MainScene.Camera.Eye) > 0)
                         {
-/*                            Vector4 TempVertexI = MainScene.GetTransformedVertex(Vertexes[FaceVertexes[0] - 1]);
+                            var triangle = Enumerable.Range(0, 3)
+                                .Select(i => MainScene.GetTransformedVertex(Vertexes[FaceVertexes[i] - 1]))
+                                .ToList();
+                            drawer.Rasterize(triangle);
+
+                            Vector4 TempVertexI = MainScene.GetTransformedVertex(Vertexes[FaceVertexes[0] - 1]);
                             Vector4 TempVertexJ = MainScene.GetTransformedVertex(Vertexes[FaceVertexes.Last() - 1]);
 
                             if ((int)TempVertexI.X > 0 && (int)TempVertexJ.X > 0 &&
@@ -208,12 +213,7 @@ namespace ObjVisualizer
                                     DrawLine((int)TempVertexI.X, (int)TempVertexI.Y, (int)TempVertexJ.X, (int)TempVertexJ.Y,
                                         pixels, stride);
                                 }
-                            }*/
-
-                            var triangle = Enumerable.Range(0, 3)
-                                .Select(i => MainScene.GetTransformedVertex(Vertexes[FaceVertexes[i] - 1]))
-                                .ToList();
-                            drawer.Rasterize(triangle);
+                            }
                         }
                     });
                 }
@@ -271,8 +271,8 @@ namespace ObjVisualizer
                 byte* pixelPtr = data + var1 * stride + var2 * 3;
 
                 *pixelPtr++ = 255;
-                *pixelPtr++ = 255;
-                *pixelPtr = 255;
+                *pixelPtr++ = 0;
+                *pixelPtr = 0;
 
                 error -= dy;
 
